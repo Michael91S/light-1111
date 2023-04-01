a6a278d8e66895f92dd3bd9d627dcb8a06e850dd
Michael Spain <m.spain@setonhill.edu>
2023-03-31 21:32:20 -0400

    Initial Commit

---
+++ light 111/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  light 111
+//
+//  Created by Michael spain on 3/31/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
